5229cea2b3524730b2be34adc9bc51ddb7ca5f1b
wingsq <wingsq520@163.com>
2020-08-16 20:46:28 +0800

    maoer

---
+++ .history/config/server_20200816203150.js
@@ -0,0 +1,14 @@
+module.exports={
+  local:{
+    open: false,
+    port: 9001,
+  },
+  http:{
+    open: true,
+    port: 80
+  },
+  https:{
+    open: false,
+    port: 443
+  }
+}
---
+++ .history/routes/api/sound_20200816111140.js
@@ -0,0 +1,24 @@
+let express = require('express')
+const koaReq     = require('request')    
+let router = express.Router()
+module.exports = function () {
+    return async function (ctx,next) {
+        // node封装的请求中间件GET请求
+        // await koaReq('http://api.douban.com/v2/movie/top250?start=25&count=2', function (error, response, body) {
+        //     if (!error && response.statusCode == 200) {
+        //         console.log(JSON.parse(body))
+        //     }
+        // })
+        await koaReq({
+            method: 'get',
+            uri: 'http://api.douban.com/v2/movie/top250',
+            qs: {
+                start: 25,
+                count: 2
+            },
+            json: true//设置返回的数据为json
+        },function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body)
+            }
+        })
